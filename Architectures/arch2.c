f6081fc0e85897c2359011ab55c3524348084407
Michael Hickman <mhickman@unm.edu>
2014-10-27 17:20:38 -0600

    arch2

--- Architectures/arch2.c
+++ Architectures/arch2.c
@@ -17,10 +17,18 @@
 
  // Architecture 2
 
-float energy[5000];
+float energy[36][5001];
+int food_lifetime[10] = {0};
+int food_freq[10] = {0};
+int max_food_eaten = 0;
+
 int red = 0;
 int blue = 0;
 int green = 0;
+
+int r = 0;
+int b = 0;
+int g = 0;
 
 void agents_controller( WORLD_TYPE *w )
 { /* Adhoc function to test agents, to be replaced with NN controller. tpc */
@@ -55,65 +63,59 @@ void agents_controller( WORLD_TYPE *w )
 	if( a->instate->metabolic_charge>0.0 )
 	{	
 		// Collision Neuron
-		int sensor[3] = {0,1,7};
-		int j;
 
-		for (j=0; j<3; j++) 
+		collision_flag = read_soma_sensor(w, a);		 	
+		skinvalues = extract_soma_receptor_values_pointer( a );
+		nsomareceptors = get_number_of_soma_receptors( a );
+
+		float weights[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+
+		int i;
+		float v_col = 0;
+		int y_col = 0;
+
+		for (i=0; i<8; i++)
+			v_col += skinvalues[i][0] * weights[i];
+
+		if (v_col > 0.0)
+			y_col = 1;
+		else
+			y_col = 0;
+
+		// Eat Neuron
+		int v_eat = 0;
+		int weight = 1;
+		v_eat = y_col * weight;
+
+		if (v_eat > 0)
 		{
-			collision_flag = read_soma_sensor(w, a);		 	
-			skinvalues = extract_soma_receptor_values_pointer( a );
-			nsomareceptors = get_number_of_soma_receptors( a );
+			delta_energy = eat_colliding_object(w, a, 0);
 
-			float weights[8] = {0, 0, 0, 0, 0, 0, 0, 0};
-			weights[sensor[j]] = 1;
-
-			int i;
-			float v_col = 0;
-			int y_col = 0;
-
-			//printf("\n--------\n");
-			for (i=0; i<8; i++)
+			if (delta_energy > 0.0)
 			{
-				v_col += skinvalues[i][0] * weights[i];
-				//printf("\tSkinValue: %f", skinvalues[i][0]);
+				//printf ("-- I ate a green!\n");
+				g += 1;
 			}
-			//printf("\n--------\n");
-
-			if (v_col > 0.0)
-				y_col = 1;
-			else
-				y_col = 0;
-
-			// Eat Neuron
-			int v_eat = 0;
-			int weight = 1;
-			v_eat = y_col * weight;
-
-
-			if (v_eat > 0)
+			else if (delta_energy < 0.0)
 			{
-				k = sensor[j];
-				delta_energy = eat_colliding_object(w, a, k);
+				//printf ("-- I ate a red!\n");
+				r += 1;
+			}
+			else if (delta_energy == 0.0)
+			{
+				//printf ("-- I ate a blue!\n");
+				b += 1;
+			}
+		}		
 
-				if (delta_energy > 0.0)
-				{
-					//printf ("-- I ate a green!\n");
-					green += 1;
-				}
-				else if (delta_energy < 0.0)
-				{
-					//printf ("-- I ate a red!\n");
-					red += 1;
-				}
-				else if (delta_energy == 0.0)
-				{
-					//printf ("-- I ate a blue!\n");
-					blue += 1;
-				}
-			}			
+
+		if (nlifetimes % 36 == 0)
+		{
+			int e_index = nlifetimes / 36;
+			energy[e_index][simtime] = a->instate->metabolic_charge;
 		}
+		//energy[simtime] = a->instate->metabolic_charge;
 
-		energy[simtime] = a->instate->metabolic_charge;
 
 		// move the agents body
 		set_forward_speed_agent( a, forwardspeed ) ;
@@ -127,9 +129,23 @@ void agents_controller( WORLD_TYPE *w )
 	} // end agent alive condition
 	else
 	{		
+		// Collect Data
 
-		//speed[nlifetimes] = forwardspeed;
-		//lifetimes[nlifetimes] = simtime;
+
+		if (r+b+g > max_food_eaten)
+		{
+			max_food_eaten = r+b+g;
+			printf("Max Food: %i", max_food_eaten);
+		}
+
+		food_lifetime[r+b+g] += simtime;
+		food_freq[r+b+g] += 1;
+
+		red += r;
+		blue += b;
+		green += g;
+
+		r = g = b = 0;
 
 		// Example of agent is dead condition
 		printf("agent_controller- Agent has died, eating %d objects. simtime: %d\n",a->instate->itemp[0], simtime ) ;
@@ -145,16 +161,20 @@ void agents_controller( WORLD_TYPE *w )
 
 		x = 0;	//distributions_uniform( Flatworld->xmin, Flatworld->xmax ) ; /* pick random starting position and heading */
 		y = 0;	//distributions_uniform( Flatworld->ymin, Flatworld->ymax ) ;
-		
 		// Slightly Rotate the agent
 		h = a->outstate->body_angle;
-		h += 20;
+		h += 1;
 
-		//h = distributions_uniform( -179.0, 179.0) ;
+
+		// x = distributions_uniform( Flatworld->xmin, Flatworld->xmax ) ; /* pick random starting position and heading */
+		// y = distributions_uniform( Flatworld->ymin, Flatworld->ymax ) ;
+		// h = distributions_uniform( -179.0, 179.0) ;
 
 		// Collect Data
 		
-		printf("Food Eaten:\nRed: %d\tBlue: %d\tGreen: %d\n", red, blue, green);
+		printf("Food Eaten:\nRed: %d\tBlue: %d\tGreen: %d\n", r, b, g);
+
+		r = g = b = 0;
 
 		printf("\nagent_controller- new coordinates after restoration:  x: %f y: %f h: %f\n",x,y,h) ;
 		set_agent_body_position( a, x, y, h ) ;    /* set new position and heading of agent */
@@ -167,21 +187,37 @@ void agents_controller( WORLD_TYPE *w )
 			avelifetime /= (float)maxnlifetimes ;
 			printf("\nAverage lifetime: %f\n",avelifetime);
 
-			printf("Food Eaten:\nRed: %d\tBlue: %d\tGreen: %d\n", red, blue, green);
+			printf("Total Food Eaten:\nRed: %d\tBlue: %d\tGreen: %d\n", red, blue, green);
 
 			// Write out data
 			
-			/*
+			// Energy vs Time
 			FILE *fp;
 			fp = fopen("./Results/Arch2 Energy vs Time.csv", "w");
 			int i;
-			for(i=0; i<simtime; i++)
+			for(i=0; i<5001; i++)
 			{
+				fprintf(fp, "%d", i);
+				int j;
+				for (j=0; j<10; j++)
+					fprintf(fp, ", %f", energy[j][i]);
+				fprintf(fp, "\n");
 				//printf("%d\n", nlifetimes);
-				fprintf(fp, "%d, %f\n", i, energy[i]);
+				//fprintf(fp, "%d, %f\n", i, energy[i]);
 			}
 			fclose(fp);
-			*/
+
+			// AvgLifetime vs Food Eaten
+			fp = fopen("./Results/Arch2 AvgLifetime vs Food", "w");
+			for (i=0; i<max_food_eaten+1; i++)
+			{
+				printf("%i, %i, %i\n", i, food_lifetime[i], food_freq[i]);
+				if (food_freq[i] > 0)
+					fprintf(fp, "%i, %i\n", i, (food_lifetime[i] / food_freq[i]));
+				else
+					fprintf(fp, "%i, %i\n", i, 0);
+			}	
+
 
 			exit(0) ;
 		}
@@ -190,6 +226,4 @@ void agents_controller( WORLD_TYPE *w )
 		
 		
 	} /* end agent dead condition */
-	
-	
 }
